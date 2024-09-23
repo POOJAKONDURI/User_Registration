dbc6dc92ccf894cd824219d0caade21f31ab7ecf
Pooja Konduri <poojakonduru15@gmail.com>
2024-09-23 12:51:23 +0530

    [pooja] Add:program to validate second name based on specific criteria

--- User_Registration.py
+++ User_Registration.py
@@ -4,34 +4,40 @@
 @Date: 23-09-2024
 @Last Modified by: Pooja 
 @Last Modified: 23-09-2024
-@Title :User registration problems UC1-User need to enter a valid First Name
+@Title :User registration problems UC2-User need to enter a valid First Name and second name
 
 '''
 import re
-def chck_firstname(first_name):
+def chck_name(name):
       '''
        Definition:
-       The check_firstname function is designed to validate a given first name based on specific criteria.
+       The check_name function is designed to validate a given second name based on specific criteria.
        Parameters:
        first_name (str): A string representing the first name to be checked.
+       second_name : string representing sedcond name to be checked
        Return:
-       1 if the first_name is at least 3 characters long and starts with an uppercase letter.
+       1 if the name is at least 3 characters long and starts with an uppercase letter.
        0 otherwise.
     
     '''
       
-      x = re.search(r'\b[A-Z]',first_name)
-      if len(first_name)>3 and x:
+      x = re.search(r'\b[A-Z]',name)
+      if len(name)>3 and x:
             return 1
       else :
             return 0
       
 def main():
       first_name = input("entr your first name: ")
-      if chck_firstname(first_name):
+      second_name = input("enter your second name: ")
+      if chck_name(first_name):
             print(f"entered first name {first_name} is valid")
       else:
-            print(f"entrd name is not valid")
+            print(f"entrd first name is not valid,It must start with a capital letter and have at least 3 characters.")
+      if chck_name(second_name):
+            print(f"entered second name {second_name} is valid") 
+      else:
+            print("enterd second name is invalid,It must start with a capital letter and have at least 3 characters.")
 
 if __name__ == "__main__":
       main()
